733d0aab7ac1606e968bd70e85f4c8175bf0a3a8
Filipino Bread <Mesa.Francis.Allen.S@gmail.com>
2025-01-23 00:20:20 +0800

    finished basic interaction modules

--- Game-Modules/Interaction-Module/interaction_area.gd
+++ Game-Modules/Interaction-Module/interaction_area.gd
@@ -1,12 +1,12 @@
 extends Area2D
-
+class_name InteractionArea
 @export var action_name: String = "interact"
 
 var interact: Callable = func():
 	pass
 
 func _on_body_entered(body):
-	pass
+	InteractionManager.register_area(self)
 
 func _on_body_exited(body: Node2D) -> void:
-	pass
+	InteractionManager.register_area(self)
--- Game-Modules/Interaction-Module/interaction_manager.gd
+++ Game-Modules/Interaction-Module/interaction_manager.gd
@@ -8,13 +8,13 @@ const base_text = "[E] to"
 
 var active_areas = []
 var can_interact = true
-#func register_area(area: InteractionArea):
-	#active_areas.push_back(area)
-#
-#func unregister_area(area: InteractionArea):
-	#var index = active_areas.find(area)
-	#if index != -1:
-		#active_areas.remove_at(index)
+func register_area(area: InteractionArea):
+	active_areas.push_back(area)
+
+func unregister_area(area: InteractionArea):
+	var index = active_areas.find(area)
+	if index != -1:
+		active_areas.remove_at(index)
 		
 func _process(delta):
 	if active_areas.size() > 0 && can_interact:
